3a1ea8ff631e054d696018b46fd71c7968e1807c
Marie <marie2004357@gmail.com>
2019-03-29 21:49:23 -0400

    Initial commit 2

---
+++ HorseOfAnotherColor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace HorseOfAnotherColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Establishing a Sql connection
+            SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="+"C:\\Users\\marie\\source\\repos\\HorseOfAnotherColor\\HorseOfAnotherColor\\Database1.mdf"+";Integrated Security=True");
+            cn.Open();
+           
+            //Requesting Input from the user
+            string HorseName;
+            Console.Write("Enter Horse Name: ");
+            HorseName = Console.ReadLine();
+
+            string Color;
+            Console.Write("Enter Horse Color: ");
+            Color = Console.ReadLine();
+
+            string Gender;
+            Console.Write("Enter Horse Gender: ");
+            Gender = Console.ReadLine();
+
+            //Inserting values into a table
+            SqlCommand cmd = new SqlCommand("insert into HorseE values('" +HorseName + "','" +Color + "','" +Gender + "')", cn);
+          
+            int i = cmd.ExecuteNonQuery();
+            if (i > 0)
+            {
+                Console.WriteLine("Insertion Successful");
+            }
+            cn.Close();
+
+
+            
+
+        }
+    }
+}
